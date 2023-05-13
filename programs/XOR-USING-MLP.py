ce15f40d097ace69e7bd3b28620c633192cd8b85
andrew264 <andrewsanthosh264@gmail.com>
2023-05-13 15:24:52 +0530

    update expt 1

--- programs/XOR-USING-MLP.py
+++ programs/XOR-USING-MLP.py
@@ -2,6 +2,9 @@ from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
 
+import tensorflow as tf
+tf.random.set_seed(69)
+
 # Define the training data
 X_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y_train = np.array([[0], [1], [1], [0]])
@@ -20,12 +23,12 @@ model.compile(loss='binary_crossentropy',
 model.fit(X_train, y_train, epochs=2000, verbose=0)
 
 # Evaluate the model
-loss, accuracy = model.evaluate(X_train, y_train)
-print(f"Loss: {loss}, Accuracy: {accuracy}")
+loss, accuracy = model.evaluate(X_train, y_train, verbose=0)
+print(f"Loss: {loss:.2f}, Accuracy: {accuracy:.2f}")
 
 # Predict the output for new input values
 X_test = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y_pred = (model.predict(X_test) > 0.5).astype("int32")
+y_pred = (model.predict(X_test, verbose=0) > 0.5).astype("int32")
 print("Input\tOutput")
 for i in range(len(X_test)):
     print(f"{X_test[i]}\t{y_pred[i][0]}")
