bf6f6fe714cf99d9802177acaf52d3e9691b6282
andrew264 <andrewsanthosh264@gmail.com>
2023-07-27 21:56:23 +0530

    not feelin good vides today

--- programs/XOR-USING-MLP.py
+++ programs/XOR-USING-MLP.py
@@ -1,17 +1,14 @@
-from keras.models import Sequential
-from keras.layers import Dense
-import numpy as np
-
 import tensorflow as tf
+from keras.layers import Dense
+from keras.models import Sequential
+
 tf.random.set_seed(69)
 
-# Define the training data
-X_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y_train = np.array([[0], [1], [1], [0]])
+X = [[0, 0], [0, 1], [1, 0], [1, 1]]
+y = [[0], [1], [1], [0]]
 
-# Define the MLP model
 model = Sequential([
-    Dense(4, input_dim=2, activation='sigmoid'),
+    Dense(4, input_dim=2, activation='relu'),
     Dense(1, activation='sigmoid')
 ])
 
@@ -19,16 +16,12 @@ model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-# Train the model
-model.fit(X_train, y_train, epochs=2000, verbose=0)
+model.fit(X, y, epochs=2000, verbose=0)
 
-# Evaluate the model
-loss, accuracy = model.evaluate(X_train, y_train, verbose=0)
+loss, accuracy = model.evaluate(X, y, verbose=0)
 print(f"Loss: {loss:.2f}, Accuracy: {accuracy:.2f}")
 
-# Predict the output for new input values
-X_test = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y_pred = (model.predict(X_test, verbose=0) > 0.5).astype("int32")
+y_pred = (model.predict(X, verbose=0) > 0.5).astype("int32")
 print("Input\tOutput")
-for i in range(len(X_test)):
-    print(f"{X_test[i]}\t{y_pred[i][0]}")
+for i in range(len(X)):
+    print(f"{X[i]}\t{y_pred[i][0]}")
