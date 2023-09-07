db0fd4a90544d0acb5a3b429b4a6f4ec3f67c3cf
Andrew Santhosh <andrewsanthosh264@gmail.com>
2023-09-08 00:11:43 +0530

    4 down, 5 more to go

--- programs/SPEECH-RECOGNITION-NLP.py
+++ programs/SPEECH-RECOGNITION-NLP.py
@@ -2,17 +2,17 @@ import os
 import random
 
 import librosa
-from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from transformers import WhisperProcessor, TFWhisperForConditionalGeneration
 
 processor = WhisperProcessor.from_pretrained("openai/whisper-base.en")
-model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-base.en")
+model = TFWhisperForConditionalGeneration.from_pretrained("openai/whisper-base.en")
 
 path = "../datasets/4-SPEECH-RECOGNITION/"
 
 audio_file = os.path.join(path, random.choice(os.listdir(path)))
 audio, sr = librosa.load(audio_file, sr=16000)
 
-input_features = processor(audio, return_tensors="pt", sampling_rate=16000).input_features
+input_features = processor(audio, return_tensors="tf", sampling_rate=16000).input_features
 generated_ids = model.generate(input_features=input_features, max_new_tokens=420)
 transcription = processor.batch_decode(generated_ids, )[0]
 print(transcription)
