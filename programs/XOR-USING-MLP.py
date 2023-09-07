9325ccc6ccd32eda957964003fd0a3ea52667fbf
Andrew Santhosh <andrewsanthosh264@gmail.com>
2023-09-07 21:49:08 +0530

    1 down 8 to go

--- programs/XOR-USING-MLP.py
+++ programs/XOR-USING-MLP.py
@@ -1,27 +1,25 @@
 import tensorflow as tf
-from keras.layers import Dense
-from keras.models import Sequential
 
 tf.random.set_seed(69)
 
-X = [[0, 0], [0, 1], [1, 0], [1, 1]]
-y = [[0], [1], [1], [0]]
+X = tf.convert_to_tensor([[0, 0], [0, 1], [1, 0], [1, 1]], )
+y = tf.one_hot([0, 1, 1, 0], depth=2, )
 
-model = Sequential([
-    Dense(8, input_dim=2, activation='relu'),
-    Dense(1, activation='sigmoid')
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(16, input_dim=2, activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax')
 ])
 
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
-
 model.fit(X, y, epochs=300, verbose=0)
 
 loss, accuracy = model.evaluate(X, y, verbose=0)
 print(f"Loss: {loss:.2f}, Accuracy: {accuracy:.2f}")
 
-y_pred = (model.predict(X, verbose=0) > 0.5).astype("int32")
-print("Input\tOutput")
-for i in range(len(X)):
-    print(f"{X[i]}\t{y_pred[i][0]}")
+y_pred = model.predict(X, verbose=0)
+y_pred = tf.argmax(y_pred, axis=1)
+print("X", "Predicted", sep='\t\t')
+for i, p in enumerate(y_pred):
+    print(f"{X[i]}", f"{p}", sep='\t\t')
